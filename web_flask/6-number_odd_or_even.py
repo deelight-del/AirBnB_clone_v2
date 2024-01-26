90446be5f6ec274e70c0bd865fe2516fa3810bf9
deelight-del <akingbenidavidoyin@gmail.com>
2024-01-26 11:02:23 +0000

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" This module contains the simple implementation of a
+simple flask program"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+# app.url_map.strict_slashes = False
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Simple HBNB route functon"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    """The route definition of display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_c(text):
+    """Simple route function to dispaly text passed in url"""
+    if (text):
+        text = text.replace("_", " ")
+        return f"C {text}"  # HTML escaping though...
+
+
+@app.route("/python/")
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text=None):
+    """Same as `display_c` but with default value and
+    using python instead of c"""
+    if text is None:
+        text = "is cool"
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_number(n):
+    """Function to display numbers that are int"""
+    # if isinstance(n, int):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """Functon to map number_template URL to """
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def display_odd_even(n):
+    """Function to display odd or even"""
+    return render_template("6-number_odd_or_even.html", number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
